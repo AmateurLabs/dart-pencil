551cdcae134aca834d86682574bd5e1210cad0ca
Joshua Thome <josh.p.thome@gmail.com>
2016-01-31 11:54:50 -0800

    Shapes

--- lib/pencil.dart
+++ lib/pencil.dart
@@ -3,6 +3,9 @@ library pencil;
 import "dart:html";
 import "dart:collection";
 import "dart:math";
+import "dart:math" as Math;
+
+part "shape.dart";
 
 class Pencil {
   CanvasElement _canvas;
@@ -14,7 +17,7 @@ class Pencil {
 
   Pencil.fromCanvas(CanvasElement ele) {
     _canvas = ele;
-    _ctx = _canvas.getContext('2d');
+    _init();
   }
 
   Pencil.createCanvas(Element parent, [int width = 800, int height = 600]) {
@@ -22,46 +25,73 @@ class Pencil {
     _canvas.width = width;
     _canvas.height = height;
     parent.append(_canvas);
+    _init();
+  }
+  
+  void _init() {
     _ctx = _canvas.getContext('2d');
+    //_ctx.translate(0.5, 0.5);
+    _ctx.imageSmoothingEnabled = false;
+    _ctx.save();
   }
   
-  Pencil clear([Object style = "#6495ED"]) => move(_canvas.width / 2, _canvas.height / 2).rectangle(_canvas.width, _canvas.height).fill(style).up();
+  static const Color _clearColor = const Color._const(100, 149, 237);
+  Pencil clear([Color style = _clearColor]) => move(_canvas.width / 2, _canvas.height / 2).box(_canvas.width, _canvas.height).fill(style).draw();
   
-  Pencil shape(List<Point> points) {
-    for (int i = 0; i < points.length; i ++) {
-      if (i == 0) _ctx.moveTo(points[i].x, points[i].y);
-      else _ctx.lineTo(points[i].x, points[i].y);
-    }
+  Pencil shape(Shape shape) {
+    _ctx.translate(shape.position.x, shape.position.y);
+    _ctx.rotate(shape.rotation*PI/180.0);
+    shape._draw(_ctx);
+    _ctx.rotate(-shape.rotation*PI/180.0);
+    _ctx.translate(-shape.position.x, -shape.position.y);
     return this;
   }
   
-  Pencil polygon(int sides, num radius) {
-    List<Point> points = new List<Point>();
-    for (int i = 0; i <= sides; i ++) points.add(new Point(radius * cos(i * 2.0 * PI / sides), radius * sin(i * 2.0 * PI / sides)));
-    return shape(points);
+  Pencil polygon(int sides, num diameter) {
+    List<Vector> points = new List<Vector>();
+    for (int i = 0; i <= sides; i ++) points.add(new Vector(diameter * 0.5 * cos(i * 2.0 * PI / sides), diameter * 0.5 * sin(i * 2.0 * PI / sides)));
+    return shape(new Polygon(points));
   }
   
-  Pencil rectangle(num width, num height) {
-    _ctx.rect(-width/2, -height/2, width, height);
+  Pencil box(num width, num height) {
+    return shape(new Box(width, height));
+  }
+  
+  Pencil square(num size) => box(size, size);
+  
+  Pencil circle(num diameter) {
+    return shape(new Circle(diameter));
+  }
+  
+  Pencil heart(num size) {
+    return shape(new Heart(size));
+  }
+  
+  Pencil text(String text, { String align:"center", String base:"middle" }) {
+    _ctx.textAlign = align;
+    _ctx.textBaseline = base;
     return this;
   }
   
-  Pencil square(num size) => rectangle(size, size);
-  
-  Pencil circle(num radius) {
-    _ctx.arc(0, 0, radius, 0, PI * 2.0);
+  Pencil sprite(Sprite sprite) {
+    sprite._draw(_ctx);
     return this;
   }
 
-  Pencil fill(Object style) {
-    _ctx.fillStyle = style;
+  Pencil fill(Color style) {
+    _ctx.fillStyle = style.toString();
     _ctx.fill();
     return this;
   }
 
-  Pencil stroke(Object style) {
-    _ctx.strokeStyle = style;
+  Pencil stroke(Color style) {
+    _ctx.strokeStyle = style.toString();
     _ctx.stroke();
+    return this;
+  }
+  
+  Pencil clip() {
+    _ctx.clip();
     return this;
   }
 
@@ -80,6 +110,8 @@ class Pencil {
     _transforms.add(op);
     return this;
   }
+  
+  Pencil spin(num angle) => rotate(angle);
 
   Pencil scale(num x, num y) {
     _Scale op = new _Scale(x, y);
@@ -87,9 +119,13 @@ class Pencil {
     _transforms.add(op);
     return this;
   }
+  
+  Pencil grow(num x, num y) => scale(x, y);
 
-  Pencil up() {
+  Pencil draw() {
     while (_transforms.length > 0) _transforms.removeLast().unapply(this);
+    _ctx.restore();
+    _ctx.save();
     _ctx.beginPath();
     return this;
   }
@@ -120,29 +156,37 @@ class _Scale extends _StackOp {
   num y;
   _Scale(this.x, this.y);
   void apply(Pencil pencil) => pencil.getContext().scale(x, y);
-  void unapply(Pencil pencil) => pencil.getContext().scale(-x, -y);
+  void unapply(Pencil pencil) => pencil.getContext().scale(1/x, 1/y);
 }
 
 class Color {
-  static const String white = "#FFFFFF";
-  static const String lightGray = "#BFBFBF";
-  static const String gray = "#7F7F7F";
-  static const String darkGray = "#3F3F3F";
-  static const String black = "#000000";
-  static const String red = "#FF0000";
-  static const String orange = "#FF7F00";
-  static const String yellow = "#FFFF00";
-  static const String lime = "#7FFF00";
-  static const String green = "#00FF00";
-  static const String turquoise = '#00FF7F';
-  static const String cyan = "#00FFFF";
-  static const String azure = "#007FFF";
-  static const String blue = "#0000FF";
-  static const String violet = "#7F00FF";
-  static const String magenta = "#FF00FF";
-  static const String rose = "#FF007F";
-
-  static String rgb(num r, num g, num b) => "rgb($r,$g,$b)";
-  static String rgba(num r, num g, num b, num a) => "rgba($r,$g,$b,$a)";
-  static String hsl(num h, num s, num l) => "hsl($h,$s,$l)";
+  final int r;
+  final int g;
+  final int b;
+  final double a;
+  
+  Color(this.r, this.g, this.b, [this.a = 1.0]);
+  const Color._const(this.r, this.g, this.b, [this.a = 1.0]);
+  
+  static const Color white = const Color._const(255, 255, 255);
+  static const Color lightGray = const Color._const(191, 191, 191);
+  static const Color gray = const Color._const(127, 127, 127);
+  static const Color darkGray = const Color._const(63, 63, 63);
+  static const Color black = const Color._const(0, 0, 0);
+  static const Color red = const Color._const(255, 0, 0);
+  static const Color orange = const Color._const(255, 127, 0);
+  static const Color yellow = const Color._const(255, 255, 0);
+  static const Color lime = const Color._const(127, 255, 0);
+  static const Color green = const Color._const(0, 255, 0);
+  static const Color turquoise = const Color._const(0, 255, 127);
+  static const Color cyan = const Color._const(0, 255, 255);
+  static const Color azure = const Color._const(0, 127, 255);
+  static const Color blue = const Color._const(0, 0, 255);
+  static const Color violet = const Color._const(127, 0, 255);
+  static const Color magenta = const Color._const(255, 0, 255);
+  static const Color rose = const Color._const(255, 0, 127);
+  static const Color transparent = const Color._const(0, 0, 0, 0.0);
+  String toString() => "rgba($r,$g,$b,$a)";
+  
+  static const List<Color> spectrum = const [red, orange, yellow, lime, green, turquoise, cyan, azure, blue, violet, magenta, rose];
 }
